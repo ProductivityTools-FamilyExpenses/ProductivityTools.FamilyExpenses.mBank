cd75c4c9778a03f1e6ef2fae4d99ca90506f48c8
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-08-29 20:03:42 +0200

    dictionary account

--- FillForms.js
+++ FillForms.js
@@ -5,12 +5,31 @@ function getSheet(trixUrl, sheetName) {
   return sheet;
 }
 
+function getAccountDictionary(trixUrl) {
+  var dictionarySheet = getSheet(trixUrl, "AccountDictionary")
+  var data = dictionarySheet.getDataRange().getValues();
+  var dict = new Object();
+  for (i = 1; i < data.length; i++) {
+    console.log(data[i])
+    dict[data[i][0]] = data[i][1]
+  }
+  console.log(dict);
+  return dict;
+}
+
 function myFunction2() {
   var trixUrl = "https://docs.google.com/spreadsheets/d/1XJAduyj-wL-kVE12Ib93htKbiEyTXuzYOG7j4BedrOA/edit?gid=0#gid=0"
   var sheet = getSheet(trixUrl, "Expenses")
 
-  var cell = sheet.getRange("F2:F");
-  // This sets the formula to be the sum of the 3 rows above B5
-  //cell.setFormulaR1C1("=VLOOKUP(R[0]C[-2];AccountConfig!A:B;2)");
-  cell.setFormulaR1C1('=IF(R[0]C[-2]!="";VLOOKUP(R[0]C[-2];AccountConfig!C[-5]:C[-4];2;false);""');
+  var accountDictionary = getAccountDictionary(trixUrl);
+  var data = sheet.getDataRange().getValues();
+  for (i = 0; i < data.length; i++) {
+    console.log(data[i])
+    if (data[i][4] == "42109862") {
+        data[i][5]="ddd"
+    }
+  }
+
+  //var cell = sheet.getRange("F2:F");
+  //cell.setFormulaR1C1('=IF(R[0]C[-2]="";"";VLOOKUP(R[0]C[-2];AccountConfig!C[-5]:C[-4];2;false))');
 }
